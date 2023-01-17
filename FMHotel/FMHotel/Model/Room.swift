61cfacbbaee46bc6ef9dd38641bafcbbb8c65d0f
Roberto D'Anna <roberto.danna97@icloud.com>
2023-01-17 16:22:12 +0100

    Room + RoomSupport Model

---
+++ FMHotel/FMHotel/Model/Room.swift
@@ -0,0 +1,29 @@
+//
+//  Room.swift
+//  FMHotel
+//
+//  Created by Roberto D'Anna on 17/01/23.
+//
+import SwiftUI
+import Foundation
+
+public struct Room:Codable{
+    var id: String
+    var isAvailable: Bool
+    var capacity: Int
+    var roomNumber: Int
+    
+    init(support:RoomSupport){
+        self.id = support.id ?? ""
+        self.capacity = support.capacity ?? 1
+        self.roomNumber = support.roomNumber ?? 1
+        self.isAvailable = Bool(truncating: (support.available ?? 0) as NSNumber)
+    }
+}
+
+public struct RoomSupport{
+    var id: String?
+    var available: Int?
+    var capacity: Int?
+    var roomNumber: Int?
+}
